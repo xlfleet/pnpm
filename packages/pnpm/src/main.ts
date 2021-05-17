3922f00a3f112ce010a6963d582484c63f4568ca
Zoltan Kochan <z@kochan.io>
2021-05-17 17:37:23 +0300

    feat: scope is optional is filters

--- packages/pnpm/src/main.ts
+++ packages/pnpm/src/main.ts
@@ -170,9 +170,11 @@ export default async function run (inputArgv: string[]) {
     config.filter = config.filter ?? []
     config.filterProd = config.filterProd ?? []
 
+    const extendFilterPattern = (filter: string) => filter.startsWith('@') ? filter : `@*/${filter}`
+
     const filters = [
-      ...config.filter.map((filter) => ({ filter, followProdDepsOnly: false })),
-      ...config.filterProd.map((filter) => ({ filter, followProdDepsOnly: true })),
+      ...config.filter.map((filter) => extendFilterPattern(filter)).map((filter) => ({ filter, followProdDepsOnly: false })),
+      ...config.filterProd.map((filter) => extendFilterPattern(filter)).map((filter) => ({ filter, followProdDepsOnly: true })),
     ]
 
     const filterResults = await filterPackages(allProjects, filters, {
